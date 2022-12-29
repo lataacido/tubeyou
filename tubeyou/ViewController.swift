18646290bd375c80d4467b3ce023c9a6ada81eb3
EDGARD <EH.ARANCIBIA@GMAIL.COM>
2022-12-28 22:23:39 -0300

    APP-CLASE-2
    ICONOS

---
+++ tubeyou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tubeyou
+//
+//  Created by Edgard Henriquez on 07-12-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
